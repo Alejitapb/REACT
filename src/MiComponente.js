4974ebb083ea10276d622c8191e86daa7df10171
Alejandra Pabón Barbosa <alejandra_pabonb@soy.sena.edu.co>
2025-04-15 08:42:15 -0500

    Importando el archivo CSS en MiComponente.js

--- src/MiComponente.js
+++ src/MiComponente.js
@@ -9,3 +9,16 @@ function MiComponente() {
     );
 }
 export default MiComponente;
+import React from 'react';
+import './MiComponente.css';
+
+function MiComponente() {
+    return (
+        <div className="mi-componente">
+            <h2>Este es mi primer componente</h2>
+            <p>¡Estoy aprendiendo React!</p>
+        </div>
+    );
+}
+
+export default MiComponente;
